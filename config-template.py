afe205d5779322343004604ffc001e97f8548a39
benpankow <pankow.ben@gmail.com>
2016-03-24 15:08:21 -0500

    Added template configuration file

---
+++ config-template.py
@@ -0,0 +1,9 @@
+# Template config file
+# Rename config.py and insert your API keys
+
+# Telegram bot access key, obtain through https://telegram.me/BotFather
+key = ""
+
+# Facebook AppID and App Secret, obtain through https://developers.facebook.com/apps
+fbid = ""
+fbsecret = ""
